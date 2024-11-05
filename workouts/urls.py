8800c1ed44ae6d50aca32930165ee1a79f120ec7
vakho <anesashviliv@mail.ru>
2024-11-05 18:37:33 +0400

    split retrieveupdatedestroy view, update urls.py

--- workouts/urls.py
+++ workouts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import WorkoutListCreateView, WorkoutRetrieveUpdateDestroyView
+from .views import WorkoutListCreateView, WorkoutRetrieveView, WorkoutUpdateView, WorkoutDestroyView
 
 urlpatterns = [
     path('workouts/', WorkoutListCreateView.as_view(), name='workout-list-create'),
-    path('workouts/<int:pk>/', WorkoutRetrieveUpdateDestroyView.as_view(), name='workout-detail-update-delete'),
+    path('workouts/<int:pk>/', WorkoutRetrieveView.as_view(), name='workout-detail'),
+    path('workouts/<int:pk>/update/', WorkoutUpdateView.as_view(), name='workout-update'),
+    path('workouts/<int:pk>/delete/', WorkoutDestroyView.as_view(), name='workout-delete')
 ]
--- workouts/views.py
+++ workouts/views.py
@@ -2,10 +2,22 @@ from rest_framework import generics
 from .models import Workout
 from .serializers import WorkoutSerializer
 
+# list create (GET, POST) view 
 class WorkoutListCreateView(generics.ListCreateAPIView):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
 
-class WorkoutRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+# retrieve (GET) view
+class WorkoutRetrieveView(generics.RetrieveAPIView):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+# update (PUT) view
+class WorkoutUpdateView(generics.UpdateAPIView):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
+
+# destroy (DELETE) view
+class WorkoutDestroyView(generics.DestroyAPIView):
     queryset = Workout.objects.all()
     serializer_class = WorkoutSerializer
